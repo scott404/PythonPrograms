94068048a3de1c65282c0d6c3775cf62ceeda42b
Scott <scott@404emporium.com>
2017-12-27 00:02:00 -0600

    Sync first files for no reason in particular than to play with git

---
+++ prog1/main.py
@@ -0,0 +1,7 @@
+#Obligitory helloWorld. Servers no purpose than to print to console "Hello world" 
+#Author: Everyone who tries to learn Python
+#Version 3.x
+print("Hello World")
+print("Not that it is fancy or anything")
+print("as these are just prints!")
+
